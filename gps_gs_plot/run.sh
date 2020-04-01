b9e57d645803545285f27419101f0082d01f019f
YZY-xiaoyuer <208366656@qq.com>
2020-04-01 18:56:41 +0800

    通过shell脚本可以选择使用opencv画图或者python画图。 目前opencv只能画2d数据 选择python画图，有三种选择： 1，画xy数据 2，画时间和两点间距离差值的数据 3，画时间，xy坐标 三维数据

--- gps_gs_plot/run.sh
+++ gps_gs_plot/run.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
 echo "开始运行run.sh"
+#===========================
 echo "进入csv文件夹"
 cd csv/
 ./csv.sh
-
+cd ..
+echo "将生成的文件拷贝到gs文件夹"
+cp csv/output/1.csv gs/
+#===========================
 echo "进入gs文件夹"
-cd ../gs/
+cd gs/
 ./gs.sh
+cd ..
+#===========================
+echo "1:opencv画图，目前只能画二维图像"
+echo "2：python画图，可以画2维和3维图像，速度较慢"
+echo "请选择画图工具"
+read SW
 
-echo "进入plot文件夹"
-cd ../plot/
-./plot.sh
+case $SW in
+1)
+echo "将生成的文件拷贝到opencv_plot文件夹" 
+cp gs/output/2.csv opencv_plot/
+echo "进入opencv_plot文件夹"
+cd opencv_plot/
+./opencv_plot.sh
+cd ..
+;;
+2)
+echo "将生成的文件拷贝到python_plot文件夹" 
+cp gs/output/2.csv python_plot/
+echo "进入python_plot文件夹"
+cd python_plot/
+./python_plot.sh
+cd ..
+;;
+esac
 
 echo "run.sh运行结束!"
 
